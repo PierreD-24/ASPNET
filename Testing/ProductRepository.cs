076ee32d9e3b30e442c078dd3919eee5216f428d
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-11-05 16:58:53 -0800

    added view products functionality

--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -9,14 +9,19 @@ namespace Testing;
 public class ProductRepository : IProductRepository
 {
     private readonly IDbConnection _conn;
-    
+
     public ProductRepository(IDbConnection conn)
     {
         _conn = conn;
     }
-    
+
     public IEnumerable<Product> GetAllProducts()
     {
         return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+    }
+
+    public Product GetProduct(int id)
+    {
+        return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
     }
 }
