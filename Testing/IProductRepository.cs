2f514405b584cc5b72615350032d8fbcab48ba2c
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-11-07 11:03:39 -0800

    completed aspnet project

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -9,5 +9,10 @@ namespace Testing
         IEnumerable<Product> GetAllProducts();
         Product GetProduct(int id);
         void UpdateProduct(Product product);
+
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
     }
 }
